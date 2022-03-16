9f266e15629f2b6169e9bff44fb6f2c313f3ee96
bojidar2584 bojidar2584 <bojidar2584@students.softuni.bg>
2022-03-16 23:36:26 +0200

    Added Custom Base Controller

---
+++ MovieGalleryApp.Web/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieGalleryApp.Web.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
