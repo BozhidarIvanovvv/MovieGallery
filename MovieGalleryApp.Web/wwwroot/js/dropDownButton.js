6e09455182791c9b11e8ace46ae87621ecba05f5
bojidar2584 bojidar2584 <bojidar2584@students.softuni.bg>
2022-03-24 19:18:41 +0200

    Added a dropdown button for profile managment.

---
+++ MovieGalleryApp.Web/wwwroot/js/dropDownButton.js
@@ -0,0 +1,9 @@
+﻿let click = document.querySelector('.button-click');
+
+let list = document.querySelector('.button-list');
+
+click.addEventListener("click", () => {
+
+    list.classList.toggle('newlist');
+
+});
