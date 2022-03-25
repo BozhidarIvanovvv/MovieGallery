e252c262e93afde807529683233973a6d6195667
bojidar2584 bojidar2584 <bojidar2584@students.softuni.bg>
2022-03-25 14:08:18 +0200

    Extended UserIdentity as ApplicationUser.

---
+++ MovieGalleryApp.Web/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieGalleryApp.Core.Constants;
+
+namespace MovieGalleryApp.Web.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = UserConstants.Roles.Administrator)]
+        public async Task<IActionResult> ManageUsers() 
+        {
+            return Ok();
+        }
+
+        //public async Task<IActionResult> CreateRole()
+        //{
+        //    await _roleManager.CreateAsync(new IdentityRole()
+        //    {
+        //        Name = "Administrator"
+        //    });
+
+        //    return Ok();
+        //}
+    }
+}
--- MovieGalleryApp.Web/Program.cs
+++ MovieGalleryApp.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieGalleryApp.Infrastructure.Data;
+using MovieGalleryApp.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
