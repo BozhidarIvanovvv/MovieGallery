64958d25a9faf62c10101828d810d117ee38278c
bojidar2584 bojidar2584 <bojidar2584@students.softuni.bg>
2022-04-11 08:06:42 +0300

    Added Movie Administrator role.

--- MovieGalleryApp.Core/Constants/UserConstants.cs
+++ MovieGalleryApp.Core/Constants/UserConstants.cs
@@ -11,6 +11,7 @@ namespace MovieGalleryApp.Core.Constants
         public static class Roles
         {
             public const string Administrator = "Administrator";
+            public const string MovieAdministrator = "Movie Administrator";
         }
 
         public static class UserEditVMConstants
